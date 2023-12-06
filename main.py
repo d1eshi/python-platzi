1f3f95e504f4971279f25b0dafeb098f14978de9
Diego Silva <silva.diegof03@gmail.com>
2023-12-06 19:38:29 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+user_option_choosed = input('rock (press 1)? papper (press 2)? scissors (press 3)?')
+pc_option =str( random.randint(1, 3))
+# rules = [[1, 1] = false]
+
+ 
+def showText():
+  return f'You choosed {user_option_choosed} and the pc choosed {pc_option}'
+
+def game():
+  if pc_option == user_option_choosed:
+    print('TIED', showText())
+  elif (user_option_choosed == 1 and pc_option == 3) or (user_option_choosed == 2 and pc_option == 1) or (user_option_choosed == 3 and pc_option == 2):
+    print('YOU WIN!!', showText())
+  else: print('LOSER', showText())
+
+game()
---
+++ strings.py
@@ -0,0 +1,14 @@
+# text = 'Ella sabe programar en Python'
+# print('Javascript' in text) #false
+# print('python' in text) #false
+# print('python' in text.lower()) #true
+
+# size = 'amor' #five size/len
+# size = 'amor  ' #six size/len
+# print(len(size))
+# print(text.swapcase()) # swap lowecase to uppercase and viceversa
+
+
+array = [{1}, {2}, {3}, {4}]
+
+print(array)
